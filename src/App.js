5075f8ef48156584b96fa269d770ba3f5457bb16
SeanKim <sean.kim84@gmail.com>
2018-09-13 21:27:56 +0700

    Practicing the gird Part 2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import "App.css";
+import "App2.css";
 
 class App extends Component {
   render() {
     return (
       <body>
-        <div id="wrapper">
-          <div className="background"></div>
-          <header>
-          <h1>Welcome to Sean PortFolio</h1>
-          <div className="navigation">
-              <ul className="list">
-                <li><a href="sean">About Me</a></li>
-                <li><a href="sean">Resume</a></li>
-                <li><a href="sean">Education</a></li>
-                <li><a href="sean">Contact</a></li>
-              </ul>
-
+        <header>
+          <div className="title">Seanstagram</div>
+          <div className="content">
+            <ul>
+              <li>AboutMe</li>
+              <li>Resume</li>
+              <li>Education</li>
+              <li>Contact</li>
+            </ul>
           </div>
-            
-          </header>
-          <div id="section">
-            <div className="section-box">unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
-            <div className="section-box">unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
-            <div className="section-box">unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
-            <div className="section-box">unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
-            <div className="section-box">unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
-            <div className="section-box">unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
+        </header>
+
+        <div id="section-a">
+          <div className="bg"></div>
+          <div className="over-content">
+            <h1>Welcome to Seanstagram. Learning ONE NEW THING Everyday</h1>
+            <p>Let's learn about each other.</p>
+            <div className="btn">Look at about ME</div>
+          </div>
+        </div>
+        <div id="section-b">
+          <div className="content">
+            <div className="box"></div>
+            <div classname="box"></div>
+            <div classname="box"></div>
+            <div classname="box"></div>
           </div>
         </div>
       </body>
